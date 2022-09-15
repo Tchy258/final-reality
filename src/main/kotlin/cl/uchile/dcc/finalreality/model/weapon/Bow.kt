81272a2ab2180c158738e7f2276ef9bffa42cfb7
Tchy258 <tchy258@yahoo.cl>
2022-09-15 04:07:16 -0300

    Class to differentiate Bows
    The class Bow is a subclass of Weapon, as it name implies, it represents a Bow.
    Bows should only be equipped by BowUsers, at present, only Engineers and Thieves are BowUsers.
    
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

---
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/weapon/Bow.kt
@@ -0,0 +1,31 @@
+package cl.uchile.dcc.finalreality.model.weapon
+
+import cl.uchile.dcc.finalreality.model.character.player.BowUser
+import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter
+import java.util.Objects
+/**
+ * A class that identifies a [Weapon] as a Bow, and tells whoever tries to equip it
+ * whether they can or not, they should be a [BowUser].
+ * @param name      the name of the weapon.
+ * @param damage    the base damage done by the weapon.
+ * @param weight    the weight of the weapon.
+ * @author <a href="https://www.github.com/Tchy258">Tchy258</a>
+ */
+class Bow(
+    name: String,
+    damage: Int,
+    weight: Int
+) : Weapon(name, damage, weight) {
+    override fun canEquip(aCharacter: PlayerCharacter): Boolean = aCharacter is BowUser
+    override fun hashCode() = Objects.hash(Bow::class, name, damage, weight)
+    override fun equals(other: Any?) = when {
+        this === other -> true
+        other !is Bow -> false
+        hashCode() != other.hashCode() -> false
+        name != other.name -> false
+        damage != other.damage -> false
+        weight != other.weight -> false
+        else -> true
+    }
+    override fun toString() = "Bow { name: '$name', damage: $damage, weight: $weight }"
+}
