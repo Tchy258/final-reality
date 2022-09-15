328043dcfc6faf3ee36bda36f0d53dc7aa818e15
Tchy258 <tchy258@yahoo.cl>
2022-09-15 04:10:20 -0300

    Class to differentiate Staves
    The class Staff is a subclass of Weapon, as it name implies, it represents a Staff.
    Unlike other weapons, staves must have a magicDamage property.
    Staves should only be equipped by StaffUsers, at present, only WhiteMages and BlackMages are StaffUsers.
    
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

---
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/weapon/Staff.kt
@@ -0,0 +1,38 @@
+package cl.uchile.dcc.finalreality.model.weapon
+
+import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter
+import cl.uchile.dcc.finalreality.model.character.player.StaffUser
+import java.util.Objects
+/**
+ * A class that identifies a [Weapon] as a Staff, and tells whoever tries to equip it
+ * whether they can or not, they should be a [StaffUser].
+ *
+ * @param name      the name of the weapon.
+ * @param damage    the base damage done by the weapon.
+ * @param weight    the weight of the weapon.
+ *
+ * @property magicDamage Int
+ *     The magicDamage of the staff.
+ *
+ * @author <a href="https://www.github.com/Tchy258">Tchy258</a>
+ */
+class Staff(
+    name: String,
+    damage: Int,
+    weight: Int,
+    val magicDamage: Int
+) : Weapon(name, damage, weight) {
+    override fun canEquip(aCharacter: PlayerCharacter): Boolean = aCharacter is StaffUser
+    override fun hashCode() = Objects.hash(Staff::class, name, damage, weight, magicDamage)
+    override fun equals(other: Any?) = when {
+        this === other -> true
+        other !is Staff -> false
+        hashCode() != other.hashCode() -> false
+        name != other.name -> false
+        damage != other.damage -> false
+        weight != other.weight -> false
+        magicDamage != other.magicDamage -> false
+        else -> true
+    }
+    override fun toString() = "Staff { name: '$name', damage: $damage, weight: $weight, magicDamage: $magicDamage }"
+}
