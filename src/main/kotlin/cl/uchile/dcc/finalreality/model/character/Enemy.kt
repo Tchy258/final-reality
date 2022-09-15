d5e73d31eafe618cd0b6fc1ec71edcde412ff2d7
Tchy258 <tchy258@yahoo.cl>
2022-09-15 03:38:12 -0300

    Added damage and toString method
    Enemies needed to have a damage property which was missing from the base code.
    The toString() method wasn't implemented and now it is.
    Code format was slightly changed to conform to ktlint norms, these changes include explicitly stating the java.util.Objects import, indentation issues and spacing issues.
    
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

--- src/main/kotlin/cl/uchile/dcc/finalreality/model/character/Enemy.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/character/Enemy.kt
@@ -1,13 +1,14 @@
 package cl.uchile.dcc.finalreality.model.character
 
 import cl.uchile.dcc.finalreality.exceptions.Require
-import java.util.*
+import java.util.Objects
 import java.util.concurrent.BlockingQueue
 
 /**
  * A class that holds all the information of a single enemy of the game.
  *
  * @param name The name of this enemy.
+ * @property damage The base damage this enemy deals.
  * @property weight The weight of this enemy.
  * @param turnsQueue The queue with the characters waiting for their turn.
  * @param maxHp The maximum health points of this enemy.
@@ -17,10 +18,11 @@ import java.util.concurrent.BlockingQueue
  *  play.
  *
  * @author <a href="https://github.com/r8vnhill">R8V</a>
- * @author ~Your name~
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
  */
 class Enemy(
     name: String,
+    val damage: Int,
     weight: Int,
     maxHp: Int,
     defense: Int,
@@ -29,15 +31,18 @@ class Enemy(
     val weight = Require.Stat(weight, "Weight") atLeast 1
 
     override fun equals(other: Any?) = when {
-        this === other                 -> true
-        other !is Enemy                -> false
+        this === other -> true
+        other !is Enemy -> false
         hashCode() != other.hashCode() -> false
-        name != other.name             -> false
-        weight != other.weight         -> false
-        maxHp != other.maxHp           -> false
-        defense != other.defense       -> false
-        else                           -> true
+        name != other.name -> false
+        damage != other.damage -> false
+        weight != other.weight -> false
+        maxHp != other.maxHp -> false
+        defense != other.defense -> false
+        else -> true
     }
 
     override fun hashCode() = Objects.hash(Enemy::class, name, weight, maxHp, defense)
+
+    override fun toString(): String = "Enemy { name:'$name', weight: $weight, maxHp: $maxHp, defense: $defense }"
 }
