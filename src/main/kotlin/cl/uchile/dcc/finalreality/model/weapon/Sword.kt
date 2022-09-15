29e307824ed5f624a6ef43f12347f780c3ccf5e8
Tchy258 <tchy258@yahoo.cl>
2022-09-15 04:11:32 -0300

    Class to differentiate Swords
    The class Sword is a subclass of Weapon, as it name implies, it represents a Sword.
    Swords should only be equipped by SwordUsers, at present, only Knights and Thieves are SwordUsers.
    
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

---
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/weapon/Sword.kt
@@ -0,0 +1,33 @@
+package cl.uchile.dcc.finalreality.model.weapon
+
+import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter
+import cl.uchile.dcc.finalreality.model.character.player.SwordUser
+import java.util.Objects
+
+/**
+ * A class that identifies a [Weapon] as a Sword, and tells whoever tries to equip it
+ * whether they can or not, they should be a [SwordUser].
+ * @param name      the name of the weapon.
+ * @param damage    the base damage done by the weapon.
+ * @param weight    the weight of the weapon.
+ * @author <a href="https://www.github.com/Tchy258">Tchy258</a>
+ */
+class Sword(
+    name: String,
+    damage: Int,
+    weight: Int
+) : Weapon(name, damage, weight) {
+
+    override fun canEquip(aCharacter: PlayerCharacter): Boolean = aCharacter is SwordUser
+    override fun hashCode() = Objects.hash(Sword::class, name, damage, weight)
+    override fun equals(other: Any?) = when {
+        this === other -> true
+        other !is Sword -> false
+        hashCode() != other.hashCode() -> false
+        name != other.name -> false
+        damage != other.damage -> false
+        weight != other.weight -> false
+        else -> true
+    }
+    override fun toString() = "Sword { name: '$name', damage: $damage, weight: $weight }"
+}
