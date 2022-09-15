74752c7b326e0fd4a02097d472f01f253b7f9407
Tchy258 <tchy258@yahoo.cl>
2022-09-15 03:51:20 -0300

    Weapon usability checks
    Five new interfaces were added to the code: SwordUser, AxeUser, KnifeUser, StaffUser and BowUser. These interfaces have no methods, but they're used to group different character classes according to the weapons they can use. This way, a Knight is also a SwordUser, an AxeUser and a KnifeUser which means a Knight can only equip those weapons and not others. Similar rules apply to other character classes.
    
    If new character classes are ever added they'd only need to implement the corresponding <Weapon>User interface to equip the weapons they should.
    Code format was slightly changed to conform to ktlint norms.
    
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

--- src/main/kotlin/cl/uchile/dcc/finalreality/model/character/player/PlayerCharacter.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/character/player/PlayerCharacter.kt
@@ -1,5 +1,5 @@
 /*
- * "Final Reality" (c) by R8V and ~Your name~
+ * "Final Reality" (c) by R8V and Tchy258
  * "Final Reality" is licensed under a
  * Creative Commons Attribution 4.0 International License.
  * You should have received a copy of the license along with this
@@ -7,9 +7,14 @@
  */
 package cl.uchile.dcc.finalreality.model.character.player
 
-import cl.uchile.dcc.finalreality.model.Weapon
 import cl.uchile.dcc.finalreality.model.character.AbstractCharacter
 import cl.uchile.dcc.finalreality.model.character.GameCharacter
+import cl.uchile.dcc.finalreality.model.weapon.Axe
+import cl.uchile.dcc.finalreality.model.weapon.Bow
+import cl.uchile.dcc.finalreality.model.weapon.Knife
+import cl.uchile.dcc.finalreality.model.weapon.Staff
+import cl.uchile.dcc.finalreality.model.weapon.Sword
+import cl.uchile.dcc.finalreality.model.weapon.Weapon
 import java.util.concurrent.BlockingQueue
 
 /**
@@ -18,17 +23,42 @@ import java.util.concurrent.BlockingQueue
  * @property equippedWeapon
  *    the weapon that the character is currently using
  * @author <a href="https://www.github.com/r8vnhill">R8V</a>
- * @author ~Your name~
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
  */
 interface PlayerCharacter {
-  val equippedWeapon: Weapon
-
-  /**
-   * Equips a weapon to the character.
-   */
-  fun equip(weapon: Weapon)
+    val equippedWeapon: Weapon
+    /**
+     * Equips a weapon to the character if it can be equipped by said character.
+     */
+    fun equip(weapon: Weapon)
 }
 
+/**
+ *  A [PlayerCharacter] that can use a [Sword]
+ *  @author <a href="https://www.github.com/tchy258">Tchy258</a>
+ */
+interface SwordUser
+
+/**
+ * A [PlayerCharacter] that can use an [Axe]
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
+ */
+interface AxeUser
+/**
+ * A [PlayerCharacter] that can use a [Knife]
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
+ */
+interface KnifeUser
+/**
+ * A [PlayerCharacter] that can use a [Staff]
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
+ */
+interface StaffUser
+/**
+ * A [PlayerCharacter] that can use a [Bow]
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
+ */
+interface BowUser
 /**
  * A class that holds all the information of a player-controlled character in the game.
  *
@@ -39,20 +69,23 @@ interface PlayerCharacter {
  * @constructor Creates a new playable character.
  *
  * @author <a href="https://www.github.com/r8vnhill">R8V</a>
- * @author ~Your name~
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
  */
 abstract class AbstractPlayerCharacter(
-  name: String,
-  maxHp: Int,
-  defense: Int,
-  turnsQueue: BlockingQueue<GameCharacter>
+    name: String,
+    maxHp: Int,
+    defense: Int,
+    turnsQueue: BlockingQueue<GameCharacter>
 ) : AbstractCharacter(name, maxHp, defense, turnsQueue), PlayerCharacter {
+    private lateinit var _equippedWeapon: Weapon
+    override val equippedWeapon: Weapon
+        get() = _equippedWeapon
 
-  private lateinit var _equippedWeapon: Weapon
-  override val equippedWeapon: Weapon
-    get() = _equippedWeapon
-
-  override fun equip(weapon: Weapon) {
-    _equippedWeapon = weapon
-  }
+    override fun equip(weapon: Weapon) {
+        if (weapon.canEquip(this)) {
+            _equippedWeapon = weapon
+        } else {
+            println("I can't equip that")
+        }
+    }
 }
