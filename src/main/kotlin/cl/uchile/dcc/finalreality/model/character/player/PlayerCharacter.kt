1fb96efa34d23ed8b79a6323b6ee31b159f7872d
Tchy258 <tchy258@yahoo.cl>
2022-09-18 23:02:43 -0300

    waitTurn implementation for PlayerCharacters
    Since player characters can only act if they do have a weapon equipped, characters with no weapons cannot "wait their turn" since they lack weight and thus, speed.
    When weapon related checks fail the names of the character and/or weapon are printed for clarity
    
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

--- src/main/kotlin/cl/uchile/dcc/finalreality/model/character/player/PlayerCharacter.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/character/player/PlayerCharacter.kt
@@ -9,63 +9,33 @@ package cl.uchile.dcc.finalreality.model.character.player
 
 import cl.uchile.dcc.finalreality.model.character.AbstractCharacter
 import cl.uchile.dcc.finalreality.model.character.GameCharacter
-import cl.uchile.dcc.finalreality.model.weapon.Axe
-import cl.uchile.dcc.finalreality.model.weapon.Bow
-import cl.uchile.dcc.finalreality.model.weapon.Knife
-import cl.uchile.dcc.finalreality.model.weapon.Staff
-import cl.uchile.dcc.finalreality.model.weapon.Sword
 import cl.uchile.dcc.finalreality.model.weapon.Weapon
 import java.util.concurrent.BlockingQueue
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 /**
  * A character controlled by the user.
  *
- * @property equippedWeapon
- *    the weapon that the character is currently using
+ * @property equippedWeapon the weapon that the character is currently using
  * @author <a href="https://www.github.com/r8vnhill">R8V</a>
  * @author <a href="https://www.github.com/tchy258">Tchy258</a>
  */
 interface PlayerCharacter {
     val equippedWeapon: Weapon
     /**
-     * Equips a weapon to the character if it can be equipped by said character.
+     * Equips a [Weapon] to a [PlayerCharacter]
      */
     fun equip(weapon: Weapon)
 }
 
 /**
- *  A [PlayerCharacter] that can use a [Sword]
- *  @author <a href="https://www.github.com/tchy258">Tchy258</a>
- */
-interface SwordUser
-
-/**
- * A [PlayerCharacter] that can use an [Axe]
- * @author <a href="https://www.github.com/tchy258">Tchy258</a>
- */
-interface AxeUser
-/**
- * A [PlayerCharacter] that can use a [Knife]
- * @author <a href="https://www.github.com/tchy258">Tchy258</a>
- */
-interface KnifeUser
-/**
- * A [PlayerCharacter] that can use a [Staff]
- * @author <a href="https://www.github.com/tchy258">Tchy258</a>
- */
-interface StaffUser
-/**
- * A [PlayerCharacter] that can use a [Bow]
- * @author <a href="https://www.github.com/tchy258">Tchy258</a>
- */
-interface BowUser
-/**
  * A class that holds all the information of a player-controlled character in the game.
  *
- * @param name        the character's name
- * @param maxHp       the character's maximum health points
- * @param defense     the character's defense
- * @param turnsQueue  the queue with the characters waiting for their turn
+ * @param name the character's name
+ * @param maxHp the character's maximum health points
+ * @param defense the character's defense
+ * @param turnsQueue the queue with the characters waiting for their turn
  * @constructor Creates a new playable character.
  *
  * @author <a href="https://www.github.com/r8vnhill">R8V</a>
@@ -76,7 +46,8 @@ abstract class AbstractPlayerCharacter(
     maxHp: Int,
     defense: Int,
     turnsQueue: BlockingQueue<GameCharacter>
-) : AbstractCharacter(name, maxHp, defense, turnsQueue), PlayerCharacter {
+) : AbstractCharacter(name, maxHp, defense, turnsQueue),
+    PlayerCharacter {
     private lateinit var _equippedWeapon: Weapon
     override val equippedWeapon: Weapon
         get() = _equippedWeapon
@@ -85,7 +56,19 @@ abstract class AbstractPlayerCharacter(
         if (weapon.canEquip(this)) {
             _equippedWeapon = weapon
         } else {
-            println("I can't equip that")
+            println("$name can't equip ${weapon.name}")
+        }
+    }
+    override fun waitTurn() {
+        if (::_equippedWeapon.isInitialized) {
+            scheduledExecutor = Executors.newSingleThreadScheduledExecutor()
+            scheduledExecutor.schedule(
+                /* command = */ ::addToQueue,
+                /* delay = */ (this.equippedWeapon.weight * 100).toLong(),
+                /* unit = */ TimeUnit.MILLISECONDS
+            )
+        } else {
+            println("$name has no weapon equipped, $name can't fight")
         }
     }
 }
