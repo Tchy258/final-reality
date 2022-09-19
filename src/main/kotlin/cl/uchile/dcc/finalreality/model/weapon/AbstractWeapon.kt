f3407a1219740b8f1d3fbef0c72c290a809e45a0
Tchy258 <tchy258@yahoo.cl>
2022-09-18 23:06:15 -0300

    Renamed Weapon to AbstractWeapon
    Since abstract classes as types are not good design (I'm still not entirely sure why), this class was renamed to AbstractWeapon for clarity and implements the Weapon interface that's used as a type instead
    
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

---
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/weapon/AbstractWeapon.kt
@@ -0,0 +1,17 @@
+package cl.uchile.dcc.finalreality.model.weapon
+
+/**
+ * A class that holds all the information of a weapon.
+ *
+ * @property name the name of the weapon.
+ * @property damage the base damage done by the weapon.
+ * @property weight the weight of the weapon.
+ *
+ * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ * @author <a href="https://www.github.com/Tchy258">Tchy258</a>
+ */
+abstract class AbstractWeapon(
+    override val name: String,
+    override val damage: Int,
+    override val weight: Int
+) : Weapon
