99fab129f02e2208fea3a908366c0bdf65332b45
Tchy258 <tchy258@yahoo.cl>
2022-09-18 23:00:42 -0300

    Documentation, implementation of waitTurn
    All enemies have weight and can wait for their turn normally
    
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

--- src/main/kotlin/cl/uchile/dcc/finalreality/model/character/Enemy.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/character/Enemy.kt
@@ -3,17 +3,18 @@ package cl.uchile.dcc.finalreality.model.character
 import cl.uchile.dcc.finalreality.exceptions.Require
 import java.util.Objects
 import java.util.concurrent.BlockingQueue
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 /**
  * A class that holds all the information of a single enemy of the game.
  *
- * @param name The name of this enemy.
- * @property damage The base damage this enemy deals.
- * @property weight The weight of this enemy.
- * @param turnsQueue The queue with the characters waiting for their turn.
- * @param maxHp The maximum health points of this enemy.
- * @param defense The defense of this enemy.
- *
+ * @param name the name of this enemy.
+ * @param turnsQueue the queue with the characters waiting for their turn.
+ * @param maxHp the maximum health points of this enemy.
+ * @param defense the defense of this enemy.
+ * @property damage the base damage this enemy deals.
+ * @property weight the weight of this enemy.
  * @constructor Creates a new enemy with a name, a weight and the queue with the characters ready to
  *  play.
  *
@@ -41,8 +42,14 @@ class Enemy(
         defense != other.defense -> false
         else -> true
     }
-
+    override fun waitTurn() {
+        scheduledExecutor = Executors.newSingleThreadScheduledExecutor()
+        scheduledExecutor.schedule(
+            /* command = */ ::addToQueue,
+            /* delay = */ (this.weight * 100).toLong(),
+            /* unit = */ TimeUnit.MILLISECONDS
+        )
+    }
     override fun hashCode() = Objects.hash(Enemy::class, name, weight, maxHp, defense)
-
     override fun toString(): String = "Enemy { name:'$name', weight: $weight, maxHp: $maxHp, defense: $defense }"
 }
