90c782d6673b7b4653314456de78007718f20895
Tchy258 <tchy258@yahoo.cl>
2022-09-18 23:04:56 -0300

    Moved weapon user interfaces to a new file
    Currently these interfaces' sole purpose is to determine which characters can equip which weapons, but maybe some specific ability or behavior could be added later to character classes that have a weapon in common
    
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

---
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/character/player/WeaponUserInterfaces.kt
@@ -0,0 +1,37 @@
+package cl.uchile.dcc.finalreality.model.character.player
+
+import cl.uchile.dcc.finalreality.model.weapon.Axe
+import cl.uchile.dcc.finalreality.model.weapon.Bow
+import cl.uchile.dcc.finalreality.model.weapon.Knife
+import cl.uchile.dcc.finalreality.model.weapon.Staff
+import cl.uchile.dcc.finalreality.model.weapon.Sword
+
+/**
+ *  A [PlayerCharacter] that can use a [Sword]
+ *  @author <a href="https://www.github.com/tchy258">Tchy258</a>
+ */
+interface SwordUser
+
+/**
+ * A [PlayerCharacter] that can use an [Axe]
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
+ */
+interface AxeUser
+
+/**
+ * A [PlayerCharacter] that can use a [Knife]
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
+ */
+interface KnifeUser
+
+/**
+ * A [PlayerCharacter] that can use a [Staff]
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
+ */
+interface StaffUser
+
+/**
+ * A [PlayerCharacter] that can use a [Bow]
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
+ */
+interface BowUser
