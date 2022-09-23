47a5aba985c7b6f2a7f77c126d7c5634af83f1af
Tchy258 <tchy258@yahoo.cl>
2022-09-22 22:55:03 -0300

    refactor: Change PlayerCharacter to GameCharacter
    Since the PlayerCharacter interface no longer exists, the NoWeaponEquippedException accepts a GameCharacter as a constructor parameter.
    Due to how the code is made, this GameCharacter is guaranteed to be an AbstractPlayerCharacter, always.
    
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

--- src/main/kotlin/cl/uchile/dcc/finalreality/exceptions/NoWeaponEquippedException.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/exceptions/NoWeaponEquippedException.kt
@@ -1,18 +1,17 @@
 package cl.uchile.dcc.finalreality.exceptions
 
 import cl.uchile.dcc.finalreality.model.character.GameCharacter
-import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter
 
 /**
  * This error is used to represent an illegal action,
  * it's shown when a character tries to join the turn queue
  * without a weapon equipped
  *
- * @constructor Creates a new [NoWeaponEquippedException] with the [PlayerCharacter] who
- * tried to enqueue itself unarmed.
+ * @constructor Creates a new [NoWeaponEquippedException] with the [GameCharacter] (controlled
+ * by the player) who tried to enqueue itself unarmed.
  *
  * @author <a href="https://github.com/Tchy258">Tchy258</a>
  */
 
-class NoWeaponEquippedException(aCharacter: PlayerCharacter) :
-    Exception("Attempted to join turn queue unarmed. ${(aCharacter as GameCharacter).name} has no weapon equipped")
+class NoWeaponEquippedException(aCharacter: GameCharacter) :
+    Exception("Attempted to join turn queue unarmed. ${aCharacter.name} has no weapon equipped")
