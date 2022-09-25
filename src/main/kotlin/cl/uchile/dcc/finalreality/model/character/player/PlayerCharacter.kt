510807a957ac5df90c38981cb0e769b9a2f65959
Tchy258 <tchy258@yahoo.cl>
2022-09-25 01:57:17 -0300

    refactor: Restore interface
    This interface is once again used to group different playable characters and gives
    a base equip method to be overriden
    
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

---
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/character/player/PlayerCharacter.kt
@@ -0,0 +1,27 @@
+/*
+ * "Final Reality" (c) by R8V and Tchy258
+ * "Final Reality" is licensed under a
+ * Creative Commons Attribution 4.0 International License.
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+package cl.uchile.dcc.finalreality.model.character.player
+
+import cl.uchile.dcc.finalreality.model.character.player.weapon.Weapon
+
+/**
+ * A character controlled by the user.
+ *
+ * @property equippedWeapon the weapon that the character is currently using
+ * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
+ */
+interface PlayerCharacter {
+    val name: String
+    val equippedWeapon: Weapon
+    /**
+     * Equips a weapon to the character.
+     * @param weapon the weapon this character equips
+     */
+    fun equip(weapon: Weapon)
+}
