4e008a683176b4de358b800a07256ad3adcf010e
Tchy258 <tchy258@yahoo.cl>
2022-09-25 01:54:24 -0300

    docs: Add project header, better description
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

--- src/main/kotlin/cl/uchile/dcc/finalreality/exceptions/NoWeaponEquippedException.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/exceptions/NoWeaponEquippedException.kt
@@ -1,17 +1,22 @@
+/*
+ * "Final Reality" (c) by R8V and Tchy258
+ * "Final Reality" is licensed under a
+ * Creative Commons Attribution 4.0 International License.
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+ */
 package cl.uchile.dcc.finalreality.exceptions
 
-import cl.uchile.dcc.finalreality.model.character.GameCharacter
+import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter
 
 /**
- * This error is used to represent an illegal action,
- * it's shown when a character tries to join the turn queue
- * without a weapon equipped
+ * This error is used to represent an illegal enqueue action
  *
- * @constructor Creates a new [NoWeaponEquippedException] with the [GameCharacter] (controlled
- * by the player) who tried to enqueue itself unarmed.
+ * @constructor Creates a new [NoWeaponEquippedException] with the [PlayerCharacter]
+ * who tried to enqueue itself unarmed.
  *
  * @author <a href="https://github.com/Tchy258">Tchy258</a>
  */
 
-class NoWeaponEquippedException(aCharacter: GameCharacter) :
+class NoWeaponEquippedException(aCharacter: PlayerCharacter) :
     Exception("Attempted to join turn queue unarmed. ${aCharacter.name} has no weapon equipped")
