e4dae4ccb6173f894903ff2c13820e920beed8e5
Nicolás Escobar Zarzar <tchy258@yahoo.cl>
2022-10-21 02:02:16 -0300

    refactor: Revert AxeTest to FunSpec
    Signed-off-by: Nicolás Escobar Zarzar <tchy258@yahoo.cl>

--- src/test/kotlin/cl/uchile/dcc/finalreality/model/character/player/weapon/AxeTest.kt
+++ src/test/kotlin/cl/uchile/dcc/finalreality/model/character/player/weapon/AxeTest.kt
@@ -1,10 +1,10 @@
 package cl.uchile.dcc.finalreality.model.character.player.weapon
 
-import io.kotest.core.spec.style.WordSpec
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.property.checkAll
 
-class AxeTest: WordSpec( {
+class AxeTest : FunSpec({
     lateinit var axe1: Axe
     lateinit var axe2: Axe
     lateinit var axe3: Axe
@@ -14,31 +14,23 @@ class AxeTest: WordSpec( {
         axe2 = Axe("TestAxe", 10, 20)
         axe3 = Axe("TestAxe2", 20, 10)
     }
-
-    "Two axes with the same parameters" should {
-        "Be equal" {
+    context("Two axes should") {
+        test("Be equal") {
             checkAll<String, Int, Int> { name, damage, weight ->
-                val axet1 =
+                val randomAxe1 =
                     Axe(name, damage, weight)
-                val axet2 =
+                val randomAxe2 =
                     Axe(name, damage, weight)
-                axet1 shouldBe axet2
+                randomAxe1 shouldBe randomAxe2
             }
             axe1 shouldBe axe2
         }
-        "Have the same hashcode" {
+        test("Have the same hashcode") {
             axe1.hashCode() shouldBe axe2.hashCode()
         }
-        "Be equal to themselves" {
-            axe1 shouldBe axe1
-            axe2 shouldBe axe2
-        }
     }
-    "Any Axe" should {
-        "Have a string representation" {
-            axe1.toString() shouldBe "Axe { name: 'TestAxe', damage: 10, weight: 20 }"
-            axe3.toString() shouldBe "Axe { name: 'TestAxe2', damage: 20, weight: 10 }"
-        }
+    test("Any Axe should have a string representation") {
+        axe1.toString() shouldBe "Axe { name: 'TestAxe', damage: 10, weight: 20 }"
+        axe3.toString() shouldBe "Axe { name: 'TestAxe2', damage: 20, weight: 10 }"
     }
-
 })
