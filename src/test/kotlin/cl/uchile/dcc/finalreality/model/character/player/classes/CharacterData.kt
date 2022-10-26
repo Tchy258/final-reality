ab81a5eb2e02dd96c4e6e783ad79d0a5eedddfbf
Tchy258 <tchy258@yahoo.cl>
2022-10-26 00:26:54 -0300

    refactor: Split character generators
    To abide by the interface segregation principle, the character generator was
    split, creating a new generator for mages, since non magic characters have no
    use for the 'maxMp' property
    
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

--- src/test/kotlin/cl/uchile/dcc/finalreality/model/character/player/classes/CharacterData.kt
+++ src/test/kotlin/cl/uchile/dcc/finalreality/model/character/player/classes/CharacterData.kt
@@ -6,39 +6,37 @@ import io.kotest.property.arbitrary.int
 import io.kotest.property.arbitrary.nonNegativeInt
 import io.kotest.property.arbitrary.positiveInt
 import io.kotest.property.arbitrary.string
-import cl.uchile.dcc.finalreality.model.character.player.classes.magical.AbstractMage
 
 /**
- * Class that contains a [PlayerCharacter]'s data
+ * Class that contains an [AbstractPlayerCharacter]'s data.
+ * This class should only be used for testing
+ *
  * @property name the character's name
  * @property maxHp the character's maxHp
- * @property maxMp the character's maxMp (if it's a class that extends [AbstractMage])
  * @property defense the character's defense
  *
  * @author <a href="https://www.github.com/tchy258">Tchy258</a>
  */
-data class CharacterData(val name: String, val maxHp: Int, val maxMp: Int, val defense: Int) {
+internal data class CharacterData(val name: String, val maxHp: Int, val defense: Int) {
     companion object {
         /**
-         * [Arb] generator for arbitrary valid [PlayerCharacter] data.
+         * [Arb] generator for arbitrary valid [AbstractPlayerCharacter] data.
          */
         val validCharacterGenerator = arbitrary {
             val name = Arb.string().bind()
             val maxHp = Arb.positiveInt().bind()
-            val maxMp = Arb.positiveInt().bind()
             val defense = Arb.nonNegativeInt().bind()
-            CharacterData(name, maxHp, maxMp, defense)
+            CharacterData(name, maxHp, defense)
         }
 
         /**
-         * [Arb] generator for arbitrary [PlayerCharacter] data, the generated characters might not be valid.
+         * [Arb] generator for arbitrary [AbstractPlayerCharacter] data, the generated characters might not be valid.
          */
         val arbitraryCharacterGenerator = arbitrary {
             val name = Arb.string().bind()
             val maxHp = Arb.int().bind()
-            val maxMp = Arb.int().bind()
             val defense = Arb.int().bind()
-            CharacterData(name, maxHp, maxMp, defense)
+            CharacterData(name, maxHp, defense)
         }
     }
 }
---
+++ src/test/kotlin/cl/uchile/dcc/finalreality/model/character/player/classes/magical/MageData.kt
@@ -0,0 +1,45 @@
+package cl.uchile.dcc.finalreality.model.character.player.classes.magical
+
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.arbitrary
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.nonNegativeInt
+import io.kotest.property.arbitrary.positiveInt
+import io.kotest.property.arbitrary.string
+
+/**
+ * Class that contains an [AbstractMage]'s data.
+ * This class should only be used for testing
+ *
+ * @property name the mage's name
+ * @property maxHp the mage's maxHp
+ * @property maxMp the mage's maxMp
+ * @property defense the mage's defense
+ *
+ * @author <a href="https://www.github.com/tchy258">Tchy258</a>
+ */
+internal data class MageData(val name: String, val maxHp: Int, val maxMp: Int, val defense: Int) {
+    companion object {
+        /**
+         * [Arb] generator for arbitrary valid [AbstractMage] data.
+         */
+        val validMageGenerator = arbitrary {
+            val name = Arb.string().bind()
+            val maxHp = Arb.positiveInt().bind()
+            val maxMp = Arb.positiveInt().bind()
+            val defense = Arb.nonNegativeInt().bind()
+            MageData(name, maxHp, maxMp, defense)
+        }
+
+        /**
+         * [Arb] generator for arbitrary [AbstractMage] data, the generated mages might not be valid.
+         */
+        val arbitraryMageGenerator = arbitrary {
+            val name = Arb.string().bind()
+            val maxHp = Arb.int().bind()
+            val maxMp = Arb.int().bind()
+            val defense = Arb.int().bind()
+            MageData(name, maxHp, maxMp, defense)
+        }
+    }
+}
