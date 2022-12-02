d34e2824ec6b918734f887c3adb2a72585994834
Tchy258 <tchy258@yahoo.cl>
2022-12-02 01:05:23 -0300

    test: GameState related exceptions test
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

--- src/test/kotlin/cl/uchile/dcc/finalreality/exceptions/InconsistentReturnStateExceptionTest.kt
+++ src/test/kotlin/cl/uchile/dcc/finalreality/exceptions/InconsistentReturnStateExceptionTest.kt
@@ -1,17 +1,5 @@
 package cl.uchile.dcc.finalreality.exceptions
 
-import cl.uchile.dcc.finalreality.controller.GameController
-import cl.uchile.dcc.finalreality.controller.state.CharacterCreationState
-import cl.uchile.dcc.finalreality.controller.state.EndCheckState
-import cl.uchile.dcc.finalreality.controller.state.EnemyDefeatedState
-import cl.uchile.dcc.finalreality.controller.state.EnemyGenerationState
-import cl.uchile.dcc.finalreality.controller.state.EnemyTurnState
-import cl.uchile.dcc.finalreality.controller.state.GameState
-import cl.uchile.dcc.finalreality.controller.state.MagicalPlayerTurnState
-import cl.uchile.dcc.finalreality.controller.state.NonMagicalPlayerTurnState
-import cl.uchile.dcc.finalreality.controller.state.PlayerDefeatedState
-import cl.uchile.dcc.finalreality.controller.state.TurnWaitState
-import cl.uchile.dcc.finalreality.controller.state.WeaponEquipState
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.property.checkAll
