2c8dde5c988d6c0ccc21695229889e96a3e9aec7
Tchy258 <tchy258@yahoo.cl>
2022-12-02 01:06:55 -0300

    refactor: tests for new implementation [WIP]
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

--- src/test/kotlin/cl/uchile/dcc/finalreality/controller/GameControllerTest.kt
+++ src/test/kotlin/cl/uchile/dcc/finalreality/controller/GameControllerTest.kt
@@ -2,16 +2,36 @@ package cl.uchile.dcc.finalreality.controller
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import java.util.Scanner
 
 class GameControllerTest : FunSpec({
     lateinit var game: GameController
-
-    context("When a GameController is created, it should") {
-        test("Request the names of 5 characters") {
-            val str = "TestCharacter1\nTestCharacter2\nTestCharacter3\nTestCharacter4\nTestCharacter5\n"
-            game = GameController(Scanner(str))
-            game.isGameOver() shouldBe false
+    ControllerRNGSeed.setSeed(6)
+    context("A GameController should") {
+        game = GameController()
+        test("Generate enemies when instantiated") {
+            // The given seed should generate exactly these enemies
+            val expectedEnemyNames: List<String> = listOf(
+                "Mutant",
+                "Were-wolf",
+                "Hooded stabber",
+                "Red panda",
+                "Basilisk"
+            )
+            game.getEnemyNames() shouldBe expectedEnemyNames
+            game.gameState.isCharacterCreation() shouldBe true
+        }
+        test("Allow only 5 characters to be created and start a fight when all 5 are present") {
+            game.startBattle() shouldBe false
+            game.createEngineer("TestEngineer") shouldBe true
+            game.startBattle() shouldBe false
+            game.createThief("TestThief") shouldBe true
+            game.startBattle() shouldBe false
+            game.createKnight("TestKnight") shouldBe true
+            game.startBattle() shouldBe false
+            game.createWhiteMage("TestWhiteMage") shouldBe true
+            game.startBattle() shouldBe false
+            game.createBlackMage("TestBlackMage") shouldBe true
+            game.startBattle() shouldBe true
         }
     }
 })
