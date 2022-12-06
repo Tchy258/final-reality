301586d4e69e39952d2994fcad6c7a1926e9057d
Tchy258 <tchy258@yahoo.cl>
2022-12-05 21:19:32 -0300

    docs: Add future main usage
    Signed-off-by: Tchy258 <tchy258@yahoo.cl>

--- src/main/kotlin/cl/uchile/dcc/finalreality/Main.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/Main.kt
@@ -3,5 +3,7 @@ package cl.uchile.dcc.finalreality
 import cl.uchile.dcc.finalreality.controller.GameController
 
 fun main() {
-    GameController()
+    val controller = GameController()
+    // This section is meant to be filled with some form of view
+    // that will use the game controller's api
 }
